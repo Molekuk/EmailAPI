1e1381cc648f44fb7381ffbf8a6a447d8576c574
Anton <anton.larin.2016@mail.ru>
2022-04-29 15:30:37 +0300

    Add project files.

---
+++ Models/EmailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace EmailAAPI.Models
+{
+    public class EmailConfiguration
+    {
+        
+        public string SmtpServer { get; set; }
+
+        
+        public string SmtpUsername { get; set; }
+
+       
+        public string SmtpPassword { get; set; }
+
+        
+        public int SmtpPort { get; set; }
+
+        
+        public bool SmtpUseSSL { get; set; }
+    }
+}
